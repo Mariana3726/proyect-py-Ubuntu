a2739dc371951dc516ec9ad0455dc33cc594d17a
Mariana3726 <marian5174@gmail.com>
2025-09-10 06:24:00 +0000

    Se crean los archivos y gráficos

--- app/charts.py
+++ app/charts.py
@@ -3,13 +3,15 @@ import matplotlib.pyplot as plt
 def generate_bar_chart(name, labels, values):
   fig, ax = plt.subplots()
   ax.bar(labels, values)
-  plt.show()
+  plt.savefig(name)
+  plt.close()
 
-def generate_pie_chart(labels, values):
+def generate_pie_chart(name, labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels)
   ax.axis('equal')
-  plt.show()
+  plt.savefig(name)
+  plt.close()
 
 if __name__ == '__main__':
   labels = ['a', 'b', 'c']
