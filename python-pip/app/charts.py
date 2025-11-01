77c19ba4ab853c63a19302eca1029cb043d06d49
Mariana3726 <marian5174@gmail.com>
2025-11-01 01:58:15 +0000

    Modificaciones en el trabajo con Docker y los archivos enlazados

--- python-pip/app/charts.py
+++ python-pip/app/charts.py
@@ -10,7 +10,7 @@ def generate_pie_chart(name, labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels)
   ax.axis('equal')
-  plt.savefig(name)
+  plt.savefig('charts_pie_es_una_prueba.png')
   plt.close()
 
 if __name__ == '__main__':
